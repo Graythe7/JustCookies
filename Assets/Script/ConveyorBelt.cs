71df7e3b85d62fab1e6a45ea80bcb291b34b1405
Graythe7 <109918660+Graythe7@users.noreply.github.com>
2025-04-22 00:08:09 +0330

    MoveBackward button and new topping counter added

--- Assets/Script/ConveyorBelt.cs
+++ Assets/Script/ConveyorBelt.cs
@@ -18,4 +18,18 @@ public class ConveyorBelt : MonoBehaviour
                 
         }
     }
+
+    public void MoveBackward()
+    {
+        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.layer == LayerMask.NameToLayer("Cookies"))
+            {
+                obj.transform.position += new Vector3(-4f, 0, 0);
+            }
+
+        }
+    }
 }
