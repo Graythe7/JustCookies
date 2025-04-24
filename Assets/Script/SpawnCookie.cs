126f9e1dd463b49eee10f919eb317e4e6a5fcc7a
Graythe7 <109918660+Graythe7@users.noreply.github.com>
2025-04-24 12:46:44 +0330

    add list of GameObjects to store the current cookie(instead of using flag to see whether it has been spawned), solved the one time cookie making issue

--- Assets/Script/SpawnCookie.cs
+++ Assets/Script/SpawnCookie.cs
@@ -5,14 +5,36 @@ using UnityEngine;
 public class SpawnCookie : MonoBehaviour
 {
     public GameObject prefabObj;
-    private bool prefabObjHasSpawned = false;
+
+    public List<GameObject> cookieParts = new List<GameObject>();
+    //private bool prefabObjHasSpawned = false;
+
 
     public void SpawnPrefab()
     {
+        /*
         if (!prefabObjHasSpawned)
         {
             Instantiate(prefabObj, transform.position, Quaternion.identity);
             prefabObjHasSpawned = true;
         }
+        */
+        GameObject lastPart = GetLastPart();
+        //Debug.Log("LastPart:" + lastPart.name);
+
+        if (lastPart == null || lastPart.tag != prefabObj.tag)
+        {
+            GameObject newPart = Instantiate(prefabObj, transform.position, Quaternion.identity);
+            cookieParts.Add(newPart);
+            //Debug.Log("cookieParts.Count: " + cookieParts.Count);
+        }
     }
+
+    public GameObject GetLastPart()
+    {
+        if (cookieParts.Count == 0) return null;
+        return cookieParts[cookieParts.Count - 1];
+    }
+
+
 }
